bf0ff14a941c8b25ae006c7076b12d9201f1f09e
Jonas <mz-martinez@live.com>
2022-05-29 17:10:04 -0300

    Add task function

--- script.js
+++ script.js
@@ -16,9 +16,44 @@ function init(){
 }
 
 const addTaskBar = ()=>{
-    let addtaskbar = document.getElementsByClassName("formCreateTask");
-    console.log(addtaskbar)
-    addtaskbar.hidden = !(addtaskbar.hidden)
+    let addtaskbar = document.getElementById("formCreateTask");
+    let addtaskbarC = document.createElement('div');
+    addtaskbarC.innerHTML = `
+    
+    <div id="formCreateTask" class="formCreateTask">
+                <span>Criar tarefa</span>
+                <form action="">
+                    <div class="topper">
+                            <div class="hora">
+
+                                <label for="">Hora</label>
+                                <input type="time" name="hora" id="">
+
+                            </div>
+                            <div class="description">
+
+                                <label for="">Tarefa</label>
+                                <input type="text" name="descricao" id="" placeholder="Arrumar a casa">
+
+                            </div>
+
+                    </div>
+                    
+                    <div class="actions">
+
+                        <button type="submit">Criar</button>
+                        <button type="reset">Cancelar</button>
+
+                    </div>
+                    
+                    
+                </form>
+                
+            </div>
+    
+    `
+    document.getElementsByClassName("box5")[0].appendChild(addtaskbarC);
+    document.getElementById('addTask').disabled = true;
 }
 
 
