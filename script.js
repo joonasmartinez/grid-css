aa816a2e2e5fa193e02f9f8acd5980f05baf3ddd
Jonas <mz-martinez@live.com>
2022-05-29 17:55:28 -0300

    Tasks adding

--- script.js
+++ script.js
@@ -15,25 +15,41 @@ function init(){
 
 }
 
+function inputEmpty(){
+    let desc = document.getElementById('inputDescription');
+    let hour = document.getElementById('inputHora');
+
+    if(hour.value == ""){
+        let input = document.getElementById('inputHora');
+        return input.focus();
+}
+    if(desc.value == ""){
+        let input = document.getElementById('inputDescription');
+        return input.focus();
+}
+return (hour.value != "" && desc.value != "")
+}
+
 const addTaskBar = ()=>{
-    let addtaskbar = document.getElementById("formCreateTask");
-    let addtaskbarC = document.createElement('div');
+        if(document.getElementsByClassName('formCreateTask')[0]) return alert("Complete o preenchimento para adicionar a tarefa.");
+        
+        let addtaskbarC = document.createElement('div');
     addtaskbarC.innerHTML = `
     
-    <div id="formCreateTask" class="formCreateTask">
+    <div class="formCreateTask">
                 <span>Criar tarefa</span>
-                <form action="">
+                <form id="formtask">
                     <div class="topper">
                             <div class="hora">
 
                                 <label for="">Hora</label>
-                                <input type="time" name="hora" id="">
+                                <input type="time" name="hora" id="inputHora">
 
                             </div>
                             <div class="description">
 
                                 <label for="">Tarefa</label>
-                                <input type="text" name="descricao" id="" placeholder="Arrumar a casa">
+                                <input type="text" name="descricao" id="inputDescription" placeholder="Arrumar a casa">
 
                             </div>
 
@@ -41,8 +57,8 @@ const addTaskBar = ()=>{
                     
                     <div class="actions">
 
-                        <button type="submit">Criar</button>
-                        <button type="reset">Cancelar</button>
+                        <button id="btnsubmit" type="submit">Criar</button>
+                        <button id="btnreset" type="reset">Cancelar</button>
 
                     </div>
                     
@@ -52,9 +68,39 @@ const addTaskBar = ()=>{
             </div>
     
     `
+
     document.getElementsByClassName("box5")[0].appendChild(addtaskbarC);
-    document.getElementById('addTask').disabled = true;
-}
+    document.getElementById('btnsubmit').addEventListener('click', (event)=>{
+        event.preventDefault();
+        if(inputEmpty()){
+            let task = document.createElement('div');
+            task.classList.add("taskBarList");
+            task.innerHTML = `
+                    <div class="taskCheck"><input type="checkbox"></div>
+                    <div class="taskbox">
+                        <div class="taskTime">
+                        ${document.getElementById('inputHora').value}
+                        </div>
+                        <div class="taskDescription">
+                        ${document.getElementById('inputDescription').value}
+                        </div>
+                        <div class="taskAction">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 80 80" style="enable-background:new 0 0 80 80;" xml:space="preserve" width="25" height="25">
+                                <g>
+                                    <polygon style="fill:#F78F8F;" points="40,49.007 15.714,73.293 6.707,64.286 30.993,40 6.707,15.714 15.714,6.707 40,30.993    64.286,6.707 73.293,15.714 49.007,40 73.293,64.286 64.286,73.293  "/>
+                                    <path style="fill:#C74343;" d="M15.714,7.414l23.578,23.578L40,31.7l0.707-0.707L64.286,7.414l8.3,8.3L49.007,39.293L48.3,40   l0.707,0.707l23.578,23.579l-8.3,8.3L40.707,49.007L40,48.3l-0.707,0.707L15.714,72.586l-8.3-8.3l23.579-23.579L31.7,40   l-0.707-0.707L7.414,15.714L15.714,7.414 M64.286,6L40,30.286L15.714,6L6,15.714L30.286,40L6,64.286L15.714,74L40,49.714L64.286,74   L74,64.286L49.714,40L74,15.714L64.286,6L64.286,6z"/>
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+            `
+            document.getElementById("taskToDo").appendChild(task);
+
+        }
+
+        
+    })
+}  
 
 
 // execuções
